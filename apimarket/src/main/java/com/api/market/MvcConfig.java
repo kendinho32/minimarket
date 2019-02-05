02af4adfe3ff7bdcd44fab2d1bbb09d6b7f58cdb
knavarro <knavarro@everis.com>
2019-02-05 18:32:58 -0300

    Actualizacion para evitar el problema con los CORS

--- apimarket/src/main/java/com/api/market/MvcConfig.java
+++ apimarket/src/main/java/com/api/market/MvcConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -12,5 +13,10 @@ public class MvcConfig implements WebMvcConfigurer {
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
 	}
 }
