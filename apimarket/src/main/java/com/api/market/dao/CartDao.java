86bfdf0efbfb12558feb35f0ff02b5fb9aa04242
knavarro <knavarro@everis.com>
2019-04-02 16:51:29 -0300

    actualizacion

--- apimarket/src/main/java/com/api/market/dao/CartDao.java
+++ apimarket/src/main/java/com/api/market/dao/CartDao.java
@@ -2,6 +2,7 @@ package com.api.market.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,5 +12,6 @@ import com.api.market.entity.Usuario;
 @Repository
 public interface CartDao extends CrudRepository<Cart, Long> {
 	
+	@Query("SELECT c FROM Cart c WHERE c.usuario = ?1 ORDER BY c.created DESC")
 	public List<?> findAllCartsByUsuario(Usuario usuario);
 }
